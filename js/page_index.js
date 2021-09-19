7ddfb209e7d065ad65fe58be7d228458fdf2a931
yk-ddm <1715858747@qq.com>
2021-09-19 15:25:45 +0800

    Site updated: 2021-09-19 15:25:37

--- js/page_index.js
+++ js/page_index.js
@@ -1,6 +1,10 @@
 document.body.onload = function() {
-    const oVeditor = document.getElementById("veditor")
-    if (oVeditor) {
-        oVeditor.placeholder = "🍈🌽欢迎留言~~~（连续评论两次我的主人才会立即收到呦） 🍦🍨"
-    }
+    var oVeditor = document.getElementById("veditor"),
+        timerId = setInterval(function () {
+        oVeditor = document.getElementById("veditor")
+        if (oVeditor) {
+            oVeditor.placeholder = "🍈🌽欢迎留言~~~（连续评论两次我的主人才会立即收到呦） 🍦🍨"
+            clearInterval(timerId)
+        }
+    }, 1000/60)
 }
