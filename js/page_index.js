17835e0c9712fda1da3866c7de152564aa2229c1
yk-ddm <1715858747@qq.com>
2021-09-19 14:24:57 +0800

    Site updated: 2021-09-19 14:24:51

--- js/page_index.js
+++ js/page_index.js
@@ -1,3 +1,5 @@
-const oVeditor = document.getElementById("veditor")
+window.onload = function() {
+    const oVeditor = document.getElementById("veditor")
 
-oVeditor.setAttribute("placeholder", "🍈🌽欢迎留言~~~（连续评论两次我的主人才会立即收到呦） 🍦🍨")
+    oVeditor.setAttribute("placeholder", "🍈🌽欢迎留言~~~（连续评论两次我的主人才会立即收到呦） 🍦🍨")
+}
