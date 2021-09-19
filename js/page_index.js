f2c2d94bca835d7d3100b086fbcb2a1bb21ae689
yk-ddm <1715858747@qq.com>
2021-09-19 15:36:56 +0800

    Site updated: 2021-09-19 15:36:50

--- js/page_index.js
+++
@@ -1,8 +0,0 @@
-var oVeditor = document.getElementById("veditor"),
-    timerId = setInterval(function() {
-        oVeditor = document.getElementById("veditor")
-        if (oVeditor) {
-            oVeditor.placeholder = "🍈🌽欢迎留言~~~（连续评论两次我的主人才会立即收到呦） 🍦🍨"
-            clearInterval(timerId)
-        }
-    }, 1000 / 60)
